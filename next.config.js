4e348e87cef2897d91a7485fb806b5c7f578585f
Lenny Mendez <lennymendez@Lennys-MacBook-Pro.local>
2018-12-31 11:45:32 -0400

    exclude node module and .next files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-module.exports = {
-  exportPathMap: function() {
-    return {
-      "/": { page: "/" }
-    };
-  }
-};
+// THE CODE BELOW IS FOR PUBLISH AS A STATIC PAGE
+// module.exports = {
+//   exportPathMap: function() {
+//     return {
+//       "/": { page: "/" }
+//     };
+//   }
+// };
+
+const withCSS = require("@zeit/next-css");
+module.exports = withCSS();
